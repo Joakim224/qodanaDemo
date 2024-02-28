f69b42ce8d9330d431f6de07fb73a774aa38dda5
Joakim224 <joakimbusiness224@gmail.com>
2024-02-28 09:53:19 +0100

    Initial commit

---
+++ src/main/java/org/example/qodanademo/QodanaDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.qodanademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QodanaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QodanaDemoApplication.class, args);
+    }
+
+}
